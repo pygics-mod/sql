71d3af8f02fb36d8d061525fbac8e3b555e09cc3
HyechurnJang <hyjang@cisco.com>
2018-05-25 11:37:09 +0900

    update

--- core.py
+++ core.py
@@ -14,15 +14,13 @@ class Sql(jzlib.LifeCycle):
     
     def __init__(self, proto):
         self._sql_proto = proto
-        self._sql_engine = proto.engine
-        self._sql_session = proto.session
         
     def __release__(self):
         self.close()
     
     def close(self):
-        self._sql_session.close_all()
-        self._sql_engine.dispose()
+        self._sql_proto.session.close_all()
+        self._sql_proto.engine.dispose()
 
 class Model(Base):
     
@@ -31,67 +29,74 @@ class Model(Base):
     id = Column(Integer, primary_key=True)
     
     @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
+    def __tablename__(cls): return cls.__name__.lower()
     
     @classmethod
     def get(cls, id):
         try:
-            with cls.__db__._sql_session.begin():
-                return cls.__db__._sql_session.query(cls).get(id)
-        except Exception as e: raise e
+            session = cls.__db__.sql_proto.session()
+            with session.begin(): return session.query(cls).get(id)
+        except Exception as e:
+            print str(e)
+            return None
     
     @classmethod
     def one(cls, *clause):
         try:
-            with cls.__db__._sql_session.begin():
-                result = cls.__db__._sql_session.query(cls)
-        except Exception as e: raise e
-        for c in clause: result = result.filter(c)
-        try: return result.first()
-        except: return None
+            session = cls.__db__.sql_proto.session()
+            with session.begin():
+                query = session.query(cls)
+                for c in clause: query = query.filter(c)
+                return query.first()
+        except Exception as e:
+            print str(e)
+            return None
     
     @classmethod
     def list(cls, *clause):
         try:
-            with cls.__db__._sql_session.begin():
-                result = cls.__db__._sql_session.query(cls)
-        except Exception as e: raise e
-        for c in clause: result = result.filter(c)
-        return result
-    
+            session = cls.__db__.sql_proto.session()
+            with session.begin():
+                query = session.query(cls)
+                for c in clause: query = query.filter(c)
+                return query
+        except Exception as e:
+            print str(e)
+            return None
+            
     @classmethod
     def count(cls, *clause):
         try:
-            with cls.__db__._sql_session.begin():
-                result = cls.__db__._sql_session.query(cls)
-        except Exception as e: raise e
-        for c in clause: result = result.filter(c)
-        return result.count()
+            session = cls.__db__.sql_proto.session()
+            with session.begin():
+                query = session.query(cls)
+                for c in clause: query = query.filter(c)
+                return query.count()
+        except Exception as e:
+            print str(e)
+            return None
     
     def create(self):
         try:
-            with self.__class__.__db__._sql_session.begin():
-                self.__class__.__db__._sql_session.add(self)
-                # self.__class__.__db__._sql_session.commit()
-                return self
-        except Exception as e: raise e
+            session = self.__class__.__db__.sql_proto.session()
+            with session.begin(): session.add(self)
+        except Exception as e: print str(e)
+        return self
         
     def update(self, **keyval):
         try:
-            with self.__class__.__db__._sql_session.begin():
+            session = self.__class__.__db__.sql_proto.session()
+            with session.begin():
                 for key, val in keyval.items(): self.__setattr__(key, val)
-                # self.__class__.__db__._sql_session.commit()
-                return self
-        except Exception as e: raise e
+        except Exception as e: print str(e)
+        return self
     
     def delete(self):
         try:
-            with self.__class__.__db__._sql_session.begin():
-                self.__class__.__db__._sql_session.delete(self)
-                # self.__class__.__db__._sql_session.commit()
-                return self
-        except Exception as e: raise e
+            session = self.__class__.__db__.sql_proto.session()
+            with session.begin(): session.delete(self)
+        except Exception as e: print str(e)
+        return self
 
 def model(sql):
     def wrapper(scheme):
--- proto.py
+++ proto.py
@@ -13,7 +13,7 @@ class Memory:
         self.proto = 'sqlite:///:memory:'
         self.engine = create_engine(self.proto)
         self.engine.execute('select 1').scalar()
-        self.session = sessionmaker(bind=self.engine, autocommit=True)()
+        self.session = sessionmaker(bind=self.engine)
 
 class File:
     
@@ -22,17 +22,17 @@ class File:
         self.proto = 'sqlite:///%s/%s.db' % (ENV.DIR.SVC, name)
         self.engine = create_engine(self.proto)
         self.engine.execute('select 1').scalar()
-        self.session = sessionmaker(bind=self.engine, autocommit=True)()
-
+        self.session = sessionmaker(bind=self.engine)
+    
 class Mysql:
     
     def __init__(self, host, username, password, root_name=''):
         _, name = pmd()
-        name = name.replace('.', '_') 
+        name = name.replace('.', '_')
         if root_name: name = '%s_%s' % (root_name, name)
         self.proto = 'mysql://%s:%s@%s' % (username, password, host)
         self.engine = create_engine(self.proto)
         self.engine.execute('select 1').scalar()
         self.engine.execute("CREATE DATABASE IF NOT EXISTS %s CHARACTER SET utf8;" % name)
         self.engine.execute("use %s;" % name)
-        self.session = sessionmaker(bind=self.engine, autocommit=True)()
+        self.session = sessionmaker(bind=self.engine)
